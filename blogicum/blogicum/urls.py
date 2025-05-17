dc0e249a0ad11ba2d436a677c932872d888e1fc0
Your Name <you@example.com>
2025-05-17 20:27:00 +0300

    Проведена работа с settings.py и корневым urls.py для корректного отображения содержимого  папки 'static'

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include, reverse_lazy
+from django.views.generic import CreateView
+from blog.forms import UserForm
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
+handler403 = 'pages.views.csrf_failure'
+
+urlpatterns = [
+    path('', include('blog.urls'), name='blog'),
+    path('pages/', include('pages.urls')),
+    path('admin/', admin.site.urls),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserForm,
+            success_url=reverse_lazy('blog:index')
+        ),
+        name='registration'
+    ),
+    path('auth/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
---
+++ blogicum/pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def about(request):
+    return render (request, 'pages/about.html')
+def rules(request):
+    return render(request, 'pages/rules.html')
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
+def csrf_failure(request, reason=""):
+    return render(request, 'pages/403csrf.html', status=403)
+class StaticPageView(TemplateView):
+    pass
